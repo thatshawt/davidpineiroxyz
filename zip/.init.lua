7b927449f7bb97cc7ca9a867291c7fcfc3cc21f1
David <david@davidpineiro.xyz>
2026-03-24 19:53:11 -0400

    new badge :P, added footer server start date.

--- zip/.init.lua
+++ zip/.init.lua
@@ -6,7 +6,50 @@ local function ends_with(str, ending)
    return ending == "" or str:sub(-#ending) == ending
 end
 
+local function exec(prog, args, env)
+	reader, writer = assert(unix.pipe())
+	if assert(unix.fork()) == 0 then
+		unix.close(1)
+		unix.dup(writer)
+		unix.close(writer)
+		unix.close(reader)
+		unix.execve(prog, args, env)
+		unix.exit(127)
+	else
+		unix.close(writer)
+
+		returnStr = ""
+
+		while true do
+			data, err = unix.read(reader)
+			if data then
+				if data ~= '' then
+					returnStr = returnStr..data
+				else
+					break
+				end
+			elseif err:errno() ~= EINTR then
+				Log(kLogWarn, tostring(err))
+				break
+			end
+		end
+		assert(unix.close(reader))
+		assert(unix.wait())
+
+		return returnStr
+	end
+end
+
 local fm = require "fullmoon"
+local unix = require 'unix'
+
+date = assert(unix.commandv('date'))
+local dateString = exec(date)
+dateString = dateString:sub(1, #dateString - 1) -- remove last char
+
+Log(kLogInfo, "got date string '%s'" % {dateString})
+
+fm.setTemplate("printStartDate", dateString)
 
 -- .fmt files loaded from /views/ folder
 local thing = fm.setTemplate({"/views/", fmt = "fmt"})
